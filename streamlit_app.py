47973fe5e181e5d80b73c5ba49a041a87df38220
shankom <45407409+shankom@users.noreply.github.com>
2024-03-06 08:29:15 +0000

    enviro

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from pinecone import Pinecone
 
 
 openapikey = os.getenv('openai')
+print(openapikey)
 # Initialize Pinecone client
 pc = Pinecone(api_key="10c85a4c-db09-45a6-93dd-2e302ba1b7cb")
 index = pc.Index("canopy--document-uploader")
